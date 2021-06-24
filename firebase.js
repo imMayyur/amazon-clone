0225f7306605ec0ca0be9c954b8585a6cafa132e
Mayur Patel <mayurp@cloudthat.com>
2021-06-24 15:05:29 +0530

    Authentication Flow

---
+++ firebase.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase'
+const firebaseConfig = {
+    apiKey: "AIzaSyD31IXpVJtvlNhyKXq08xshpKxMLCfji6I",
+    authDomain: "mayyur-com.firebaseapp.com",
+    projectId: "mayyur-com",
+    storageBucket: "mayyur-com.appspot.com",
+    messagingSenderId: "326225740766",
+    appId: "1:326225740766:web:5509a23f7fa0260210d34f"
+  };
